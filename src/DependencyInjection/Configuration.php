4c15def4f2059c0de50fb58e2f169d5c95d0c9e7
adapik <adapik@yandex.ru>
2020-02-11 10:35:36 +0300

    allow symfony 5.0

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -29,8 +29,13 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('yamilovs_sms');
+        $treeBuilder = new TreeBuilder('yamilovs_sms');
+        if (\method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('yamilovs_sms');
+        }
 
         $this->buildProviderConfiguration($rootNode);
 
